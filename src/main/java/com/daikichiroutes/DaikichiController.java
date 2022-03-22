93e2a22a80d1ec6ec3334c6cbacaaf48ba66910d
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2022-03-22 10:22:53 -0400

    Added two new routes

--- src/main/java/com/daikichiroutes/DaikichiController.java
+++ src/main/java/com/daikichiroutes/DaikichiController.java
@@ -2,10 +2,11 @@ package com.daikichiroutes;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController("/daikichi")
+@RestController("/")
 public class DaikichiController {
-	@RequestMapping("")
+	@RequestMapping("/daikichi")
 	public String daikichi() {
 		return "Welcome!";
 	}
@@ -18,6 +19,26 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	
+	@RequestMapping("/m/daikichi/{travel}")
+	public String showTravel( 
+			@PathVariable("travel") String travel ) {
+		return "Congratulations you will travel to " + travel;
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable int num){
+		if(num % 2 == 0) {
+			return "You will take a grand journey in the near future" ;
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now it's a great time to spend time with friends and family";
+		}
+		
+		
+	}
+	
+	
 	
 	
 
